02ade1db6e119303b0a086094ec3e3efca6ecf79
Dashtikh <56628256+Dashtikh@users.noreply.github.com>
2019-11-21 12:54:17 +0330

    base layeout

---
+++ src/com/company/Loginpage.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Loginpage {
+    public Loginpage() {
+        JFrame jFrame = new JFrame("Login Page");
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setSize(400,600);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+
+
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,20 +2,39 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(()-> new Main().Startup());
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        /*Class.forName("oracle.jdbc.driver.OracleDriver");
+        Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "Dashtikh","dashti1565");
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into Users (id,username,password) values (?,?,?)");
+        preparedStatement.setLong(1, 2);
+        preparedStatement.setString(2, "parham");
+        preparedStatement.setLong(3, 1234);
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+        connection.close();*/
+        //SwingUtilities.invokeLater(()-> new Main().Startup());
+        SwingUtilities.invokeLater(()-> new Loginpage());
+
     }
-     public void Startup(){
+
+
+    public void Startup(){
         JLabel greeting = new JLabel("hello world",JLabel.CENTER);
-        JFrame frame = new JFrame("important message !");
+
+        JFrame frame = new JFrame("Restaurant Management");
         frame.setIconImage(Toolkit.getDefaultToolkit().getImage(Main.class.getResource("45332.PNG")));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(greeting);
-        frame.setSize(240,160);
+        frame.setSize(1080,720);
         frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
 
 
     }
